c099dd44d1b78ab65d0fbae4ae59589833a78b14
Michael Szeto <szetomj@gmail.com>
2017-05-02 21:35:20 +1200

    Wrapped event handlers into a if statement to prevent target class to return NULL
    Wrapped event handlers into a if statement to prevent target class to
    return NULL

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,10 +1,12 @@
 var btnToggle = document.querySelectorAll('div.btn-default');
 
-for(var i = 0; i < btnToggle.length; i++){
-	btnToggle[i].addEventListener('click', toggleLoad, false);
+if(btnToggle){
+	for(var i = 0; i < btnToggle.length; i++){
+		btnToggle[i].addEventListener('click', toggleLoad, false);
 
-	function toggleLoad(){
-		var spinner = document.getElementsByClassName('spinner-container');
-		spinner[0].style.display = "block";
+		function toggleLoad(){
+			var spinner = document.getElementsByClassName('spinner-container');
+			spinner[0].style.display = "block";
+		}
 	}
 }
